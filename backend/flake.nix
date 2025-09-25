dc1d7afd0237da0a2fd97bdb27fb6ad94848d13f
nboj <60493251+nboj@users.noreply.github.com>
2025-09-25 18:55:39 -0400

    refactor

--- backend/flake.nix
+++ backend/flake.nix
@@ -7,33 +7,22 @@
     inputs.systems.follows = "systems";
   };
 
-  outputs =
-    { nixpkgs, flake-utils, ... }:
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
     flake-utils.lib.eachDefaultSystem (
-      system:
-      let
+      system: let
         pkgs = nixpkgs.legacyPackages.${system};
-        fhs = pkgs.buildFHSEnv {
-          name = "pipezone";
-          targetPkgs = pkgs: ([
-            pkgs.bashInteractive 
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.bashInteractive
             pkgs.python312
             pkgs.python312Packages.virtualenv
             pkgs.python312Packages.pip
-          ]); 
-          runScript = "zsh";
-        };
-
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          shellHook = ''
-            if [ -z "\$\{IN_FHS_ENV-}" ]; then
-              export IN_FHS_ENV=1
-              export DIRENV_DISABLE=1
-              exec ${fhs}/bin/${fhs.name}
-            fi
-          '';
+          ];
         };
       }
     );
