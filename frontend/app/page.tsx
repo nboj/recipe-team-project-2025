de0fecbfdcc0fe843c035dc4cef98a9b5cedaa9e
nboj <60493251+nboj@users.noreply.github.com>
2025-10-30 15:06:11 -0400

    u

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,22 @@
+'use client'
+import { useState } from "react";
+
+
 export default function Home() {
+  const [variable, setVariable] = useState<number>(0);
+  const increaseNumber = () => {
+    setVariable(previous => previous + 1)
+  }
+  const decreaseNumber = () => {
+    setVariable(previous => previous - 1)
+  }
   return (
     <main>
       <h1>Hello, World</h1>
+      <p>this is the current variable: {variable}</p>
+      <button onClick={increaseNumber}>(increase +)</button>
+      <p></p>
+      <button onClick={decreaseNumber}>(decrease -)</button>
     </main>
   );
 }
