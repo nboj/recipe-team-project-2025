43197b91db459c4080cc8dd89fd63a1dc6da1a48
Shaemon Leach <Sleach12@kent.edu.com>
2025-11-06 11:15:09 -0500

    updates to frontend

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,14 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "source.unsplash.com" },
+      { protocol: "https", hostname: "images.unsplash.com" },
+    ],
+  },
 };
 
 export default nextConfig;
+
